a7b7ee3ff9cba855628e989b291efb7d1c71aac3
oscar-hakkinen <Hako1_15@uni.worc.ac.uk>
2017-10-19 14:45:46 +0100

    test 1

--- www/js/index.js
+++ www/js/index.js
@@ -1,45 +1,74 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
+//var paused_count =0;
+//var resumed_count = 0;
+//var launched_count = 0;
 
-document.addEventListener("deviceready", onDeviceReady, false);
+var key = "phone";
+var value = "samsung";
+
+window.localStorage.setItem(phone,samsung);
+
+
+var key = "bottle";
+var value = "cola";
+
+window.localStorage.setItem(bottle,cola);
+
+var key = "coin";
+var value = "pound";
+
+window.localStorage.setItem(coin,pound);
+
+var key = "headphones";
+var value = "white";
+
+window.localStorage.setItem(headphones,white);
+
+var key = "watch";
+var value = "blue";
+
+window.localStorage.setItem(watch,blue);
+
+window.alert("this is an alert");
+
+
+//document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
-function updateDisplay() {
+//function updateDisplay() {
 
-	$("#launched").text("Application launched: " + launched_count);
-	$("#resumed").text("Application paused: " + paused_count);
-	$("#paused").text("Application resumed: " + resumed_count);
-}
+	//$("#launched").text("Application launched: " + launched_count);
+	//$("#resumed").text("Application paused: " + paused_count);
+	//$("#paused").text("Application resumed: " + resumed_count);
+//}
 
 
 // device APIs are available
 //
-    function onDeviceReady() {
+  //  function onDeviceReady() {
 	
-	document.addEventListener("resume", onResume, false);
-	document.addEventListener("pause", onPause, false);
+//	document.addEventListener("resume", onResume, false);
+//	document.addEventListener("pause", onPause, false);
 	
-	launched_count++;
-	updateDisplay();
+//	launched_count++;
+//	updateDisplay();
 	    
-	alert("device ready");
-    }
+//	alert("device ready");
+//  }
 
 
-    function onPause() {
+//    function onPause() {
 	
-	paused_count++;
-	updateDisplay();
+//	paused_count++;
+//	updateDisplay();
 	    
-	alert("pause");
-    }
+//	alert("pause");
+  //  }
 	
 
-    function onResume() {
+    //function onResume() {
 		
-	resumed_count++;
-	updateDisplay();
+//	resumed_count++;
+//	updateDisplay();
 	    
-	alert("resume");
-    }
+//	alert("resume");
+//    }
